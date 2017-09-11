217b191c50ac3401d9ea4a3d2499898d03719fdb
Jerry Song <ostrichy@outlook.com>
2017-09-11 16:15:14 +0800

    update script

--- Tools/replace.sh
+++ Tools/replace.sh
@@ -2,13 +2,15 @@
 echo Replace started!
 
 echo Apply CDN
-Source="/static/"
-Target="http://lightcdn.azurewebsites.net/static/"
+Source="\/static\/"
+Target="http:\/\/lightcdn.azurewebsites.net\/static\/"
 echo replace "$Source" with "$Target"
 
 for file in $(ls ./dist/build.*)
 do
     echo Working on "$file"
-
-    sed -i '' 's|'"$Source"'|'"$Target"'|g' "$file"
+    sed 's/'"$Source"'/'"$Target"'/g' < "$file" > "${file}.tmp"
+    cp "${file}.tmp" "$file"
+    rm "${file}.tmp"
+    #sed -i '' 's|'"$Source"'|'"$Target"'|g' "$file"
 done
