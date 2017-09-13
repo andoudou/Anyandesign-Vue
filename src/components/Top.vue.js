ae90b12e8111f5c5666b91729410d1662101a353
Jerry Song <ostrichy@outlook.com>
2017-09-13 16:20:14 +0800

    Tune scroll speed

--- src/components/Top.vue.js
+++ src/components/Top.vue.js
@@ -3,7 +3,8 @@ export default {
   methods: {
     goTop: function () {
       var top = typeof window.scrollY === 'undefined' ? window.pageYOffset : window.scrollY
-      this.$scrollTo('#app', top / 4, {easing: [0.785, 0.135, 0.15, 0.86]})
+      // Scroll takes at least 1 second. The longer the page is, the longer time will be taken
+      this.$scrollTo('#app', top / 4 > 1000 ? top / 4 : 1000, {easing: [0.785, 0.135, 0.15, 0.86]})
     }
   }
 }
